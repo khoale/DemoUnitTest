c7513fea8db0b842f2d5601c1062d35a2fe84b4b
khoalea <Khoa.LeAnh@harveynash.vn>
2015-10-29 09:40:55 +0700

    Add integration test

---
+++ DemoUnitTest/DemoUnitTest.Tests/NumberServiceIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Moq;
+using Xunit;
+
+namespace DemoUnitTest.Tests
+{
+    public class NumberServiceIntegrationTest
+    {
+        private readonly NumberService _sut;
+
+        public NumberServiceIntegrationTest()
+        {
+            _sut = new NumberService(new NumberProvider());
+        }
+
+        [InlineData("Samples/PerfectFile.txt", true)]
+        [InlineData("Samples/InPerfectFile.txt", false)]
+        [Theory]
+        public void IsPerfect(string fileName, bool expectedIsPerfect)
+        {
+            var result = _sut.IsPerfect(fileName);
+            Assert.Equal(expectedIsPerfect, result);
+        }
+    }
+}
